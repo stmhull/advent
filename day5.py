63a7a7d593aeef93b668b0d9abe3dbb4bc2e764d
Stephen Hull <stm.hull@gmail.com>
2020-12-09 12:18:19 -0500

    day 5

---
+++ day5.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+from functools import reduce
+
+with open("day5_input.txt", "r") as f:
+    lines = f.readlines()
+    seats = list(map(lambda x: "".join(x.split()), lines))
+
+
+def findValue(seat, range):
+    lo = "F" if ("F" in seat or "B" in seat) else "L"
+    
+    if len(seat) == 1:
+        return range[0] if seat == lo else range[1]
+    
+    if seat[0] == lo:
+        return findValue(seat[1:], range[:(int(len(range) / 2))])
+    
+    return findValue(seat[1:], range[(int(len(range) / 2)):])
+
+
+def part1():
+    max = 0
+    for seat in seats:
+        row = findValue(seat[:7], range(0, 128))
+        column = findValue(seat[7:], range(0, 9))
+        max = (row * 8) + column if (row * 8) + column > max else max
+    
+    return max
+
+def part2():
+    ids = [(findValue(seat[:7], range(0, 128)) * 8) + findValue(seat[7:], range(0, 9)) for seat in seats]
+    id_range = {x for x in range(48, 819)}
+    return id_range.difference(ids)
+
+
+print("part 1:", part1())
+print("part 2:", part2())
